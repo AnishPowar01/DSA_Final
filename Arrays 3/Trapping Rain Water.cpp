97219761eea6235666af6c07c0e05844bc2a5fb3
AnishPowar01 <powaranish74@gmail.com>
2023-06-13 09:58:32 +0530

    Added - Optimal Approach

--- Arrays 3/Trapping Rain Water.cpp
+++ Arrays 3/Trapping Rain Water.cpp
@@ -43,3 +43,65 @@ public:
         return sum;
     }
 };
+
+
+
+// Approach 2 : 
+
+// Time Complexity :  O(N);
+// Space Complexity : O(1);
+
+
+class Solution{
+    public:
+    int trap(vector<int>& height)
+    {
+        int n = height.size();
+        int left = 0;
+        int right = n-1;
+        int ans = 0;
+        int leftMax = 0;
+        int rightMax = 0;
+
+
+        while(left<=right)
+        {
+            if(height[left] <= height[right]) 
+            {
+                // consider left side.. 
+
+                if(height[left] >= leftMax)
+                {
+                    leftMax = height[left];
+                }
+                else
+                {
+                    ans += leftMax - height[left];
+                }
+
+                left++;
+            }
+
+            else
+            {
+
+                 if(height[right] >= rightMax)
+                {
+                    rightMax = height[right];
+                }
+                else
+                {
+                    ans += rightMax - height[right];
+                }
+
+                right--;
+
+            }
+
+
+        }
+
+        return ans;
+    }
+};
+

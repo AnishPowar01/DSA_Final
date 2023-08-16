61fba88cc0472824b165d0357bba4cfc18fe8ae0
AnishPowar01 <powaranish74@gmail.com>
2023-08-16 23:37:27 +0530

    Binary_search

---
+++ Binary Search/Basics/bs.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int start = 0;
+        int end = nums.size()-1;
+        int ans = -1;
+
+        while(start < end)
+        {
+            int mid = start + (end -start)/2;
+
+            if(nums[mid] == target)
+            {
+
+            ans = mid;
+
+            end = mid;
+
+            }
+
+            else
+            {
+                start = mid +1;
+            }
+        }
+
+        return ans;
+    }
+};
